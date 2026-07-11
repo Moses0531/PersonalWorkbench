ab9f7524b8b9557173fe65a8549cd99f093d0186
mosesmoody <13612345678@163.com>
2026-07-11 12:37:07 +0800

    添加start-module的启动类代码，实现该项目正常启动

---
+++ springboot-module/start-module/src/main/java/com/moses/RunApplication.java
@@ -0,0 +1,15 @@
+package com.moses;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class RunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+    }
+}
