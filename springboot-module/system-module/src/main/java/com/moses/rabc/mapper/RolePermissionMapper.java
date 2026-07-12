d76af55806cd71a5cc7d9752063018f874f01741
FQM <13612345678@163.com>
2026-07-12 13:47:27 +0800

    创建角色与权限的自定义查询方法（根据roleId）

--- springboot-module/system-module/src/main/java/com/moses/rabc/mapper/RolePermissionMapper.java
+++ springboot-module/system-module/src/main/java/com/moses/rabc/mapper/RolePermissionMapper.java
@@ -1,15 +1,23 @@
 package com.moses.rabc.mapper;
 
 import com.moses.rabc.entity.RolePermission;
+import com.moses.rabc.entity.SysPermission;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
 * @author Administrator
 * @description 针对表【role_permission(角色-权限关联表)】的数据库操作Mapper
 * @createDate 2026-07-12 12:04:43
-* @Entity com.moses.manager.entity.RolePermission
+* @Entity com.moses.rabc.entity.RolePermission
 */
 public interface RolePermissionMapper extends BaseMapper<RolePermission> {
+
+    List<SysPermission> selectPermissionsByRoleId(@Param("roleId") Long roleId);
+
+    int deleteByRoleIdAndPermissionId(@Param("roleId") Long roleId, @Param("permissionId") Long permissionId);
 
 }
 
