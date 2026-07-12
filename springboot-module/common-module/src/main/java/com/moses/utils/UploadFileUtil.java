aa081dd926b5fe10a28d629d6f2906e0fec49659
FQM <13612345678@163.com>
2026-07-12 21:58:58 +0800

    合并本地上传文件的工具类与上传阿里云工具类

--- springboot-module/common-module/src/main/java/com/moses/utils/UploadFileUtil.java
+++ springboot-module/common-module/src/main/java/com/moses/utils/UploadFileUtil.java
@@ -1,18 +1,37 @@
 package com.moses.utils;
 
 import com.moses.config.ResultConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.util.UUID;
 
-@RestController
-@RequestMapping("/commons")
-public class UploadFileToLocalUtil {
-    private static final String UPLOAD_DIR = "D:/images/";
+@Component
+public class UploadFileUtil {
+
+    @Autowired
+    private AliCouldUtil aliyunOSSOperator;
+
+    /**
+     * 上传文件 - 阿里云
+     */
+    public ResultConfig uploadToAliYun(MultipartFile file) throws Exception {
+        if (!file.isEmpty()) {
+            // 生成唯一文件名
+            String originalFilename = file.getOriginalFilename();
+            String extName = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String uniqueFileName = UUID.randomUUID().toString().replace("-", "") + extName;
+            // 上传文件
+            String url = aliyunOSSOperator.upload(file.getBytes(), uniqueFileName);
+            return ResultConfig.success(url);
+        }
+        return ResultConfig.error("上传失败");
+    }
+
+
     /**
      * 上传文件 - 参数名file
      */
@@ -24,7 +43,7 @@ public class UploadFileToLocalUtil {
             String extName = originalFilename.substring(originalFilename.lastIndexOf("."));
             String uniqueFileName = UUID.randomUUID().toString().replace("-", "") + extName;
             // 拼接完整的文件路径
-            File targetFile = new File(UPLOAD_DIR + uniqueFileName);
+            File targetFile = new File("D:/files/" + uniqueFileName);
 
             // 如果目标目录不存在，则创建它
             if (!targetFile.getParentFile().exists()) {
